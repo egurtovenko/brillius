6a6360f91c23939c398c635838af314a4fb170aa
egurtovenko <gurtovenko030984@gmail.com>
2016-09-14 19:38:52 +0300

    circlefill add

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,54 @@
 //= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
 //= ../../bower_components/wow/dist/wow.js
-//= ../../bower_components/jquery-circle-progress/dist/circle-progress.js
+//= ../../bower_components/circliful/js/jquery.circliful.js
+
+$( document ).ready(function() {
+	$("#web-circle").circliful({
+        animationStep: 3,
+        backgroundBorderWidth: 15,
+        percent: 90,
+        halfCircle: 1,
+        foregroundColor: '#fff',
+        backgroundColor: 'rgba(0,0,0,0.26)',
+        replacePercentageByText: 'Web Design',
+        percentageTextSize: '14',
+        fontColor: '#fff',
+        animateInView: true
+    });
+    $("#print-circle").circliful({
+        animationStep: 3,
+        backgroundBorderWidth: 15,
+        percent: 75,
+        halfCircle: 1,
+        foregroundColor: '#fff',
+        backgroundColor: 'rgba(0,0,0,0.26)',
+        replacePercentageByText: 'Print Design',
+        percentageTextSize: '14',
+        fontColor: '#fff',
+        animateInView: true
+    });
+    $("#brand-circle").circliful({
+        animationStep: 3,
+        backgroundBorderWidth: 15,
+        percent: 50,
+        halfCircle: 1,
+        foregroundColor: '#fff',
+        backgroundColor: 'rgba(0,0,0,0.26)',
+        replacePercentageByText: 'Branding',
+        percentageTextSize: '14',
+        fontColor: '#fff',
+        animateInView: true
+    });
+    $("#motion-circle").circliful({
+        animationStep: 3,
+        backgroundBorderWidth: 15,
+        percent: 25,
+        halfCircle: 1,
+        foregroundColor: '#fff',
+        backgroundColor: 'rgba(0,0,0,0.26)',
+        replacePercentageByText: 'Motion',
+        percentageTextSize: '14',
+        fontColor: '#fff',
+        animateInView: true
+    });
+});
