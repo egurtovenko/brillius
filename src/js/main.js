28926578412dbcdaef7e7bedc0deb30fd8887015
egurtovenko <gurtovenko030984@gmail.com>
2016-09-14 15:51:10 +0300

    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 //= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
 //= ../../bower_components/wow/dist/wow.js
-//= ../../bower_components/circliful/js/jquery.circliful.js
+//= ../../bower_components/jquery-circle-progress/dist/circle-progress.js
